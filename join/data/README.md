421f8a965dd6d6da037ff13000d115005926f568
Roger Hoover <roger.hoover@gmail.com>
2015-03-25 15:12:58 -0700

    Script to generate fake data

---
+++ join/data/README.md
@@ -0,0 +1,5 @@
+#Setup
+* Install virtualenv
+* Create a virtualenv
+* Activate it
+* Install requirements `pip install -r requirements.txt`
---
+++ join/data/generate_users.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Generate test users.
+
+Usage:
+  generate_users.py <number> [--separator=<separator>]
+  generate_users.py (-h | --help)
+
+Examples:
+  ./generate_users.py 200 --separator=',
+  ./generate_users.py 200 --separator=$'\\t'
+
+Options:
+  -h --help     Show this screen.
+  --separator=<separator>   Separator between key and value [default: \t]
+
+"""
+from docopt import docopt
+from faker import Faker
+import json
+
+def main(num, separator):
+    fake = Faker()
+    for id in range(1, num+1):
+        print "%s%s%s" % (id, separator, json.dumps(create_user(fake)))
+
+def create_user(fake):
+    return {
+        'name': fake.name(),
+        'email': fake.email(),
+        'phone_number': fake.phone_number()
+    }
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    num = int(arguments['<number>'])
+    if num <= 0:
+        raise Exception("Number must be a positive")
+    separator = arguments['--separator']
+    main(num, separator)
