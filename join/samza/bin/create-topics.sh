2dab0099e21f9ec25d5741fa312e311afb8b536a
Roger Hoover <roger.hoover@gmail.com>
2015-03-28 13:13:48 -0700

    Added error topic

--- join/samza/bin/create-topics.sh
+++ join/samza/bin/create-topics.sh
@@ -6,5 +6,6 @@ NUM_PARTITIONS=4
 
 ${DIR}/../deploy/confluent/bin/kafka-topics --zookeeper localhost:2181 --topic users_json --create --partitions $NUM_PARTITIONS --replication-factor 1 --config "cleanup.policy=compact" --config "segment.bytes=104857600"
 ${DIR}/../deploy/confluent/bin/kafka-topics --zookeeper localhost:2181 --topic requests_json --create --partitions $NUM_PARTITIONS --replication-factor 1
+${DIR}/../deploy/confluent/bin/kafka-topics --zookeeper localhost:2181 --topic error_requests_json --create --partitions 1 --replication-factor 1
 ${DIR}/../deploy/confluent/bin/kafka-topics --zookeeper localhost:2181 --topic requests_join_user_json --create --partitions $NUM_PARTITIONS --replication-factor 1
 
